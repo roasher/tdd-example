5921abf33e15498f94703bb6d014f9368018dd3d
pyurkin <pavel.yurkin@leroymerlin.ru>
2018-04-26 14:49:29 +0300

    initial commit

---
+++ src/main/java/ru/yurkinsworkshop/tddexample/TddExampleApplication.java
@@ -0,0 +1,12 @@
+package ru.yurkinsworkshop.tddexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddExampleApplication.class, args);
+	}
+}
