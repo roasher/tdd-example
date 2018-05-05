bbfb7ab355482b497dc2c2d14167f24989ed6712
pyurkin <pavel.yurkin@leroymerlin.ru>
2018-05-05 11:27:30 +0300

    add UpdateProcessorService implementation

--- src/main/java/ru/yurkinsworkshop/tddexample/service/UpdateProcessorService.java
+++ src/main/java/ru/yurkinsworkshop/tddexample/service/UpdateProcessorService.java
@@ -1,13 +1,46 @@
 package ru.yurkinsworkshop.tddexample.service;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.yurkinsworkshop.tddexample.client.VozovozClient;
+import ru.yurkinsworkshop.tddexample.dto.ProductAvailability;
 import ru.yurkinsworkshop.tddexample.dto.Update;
+import ru.yurkinsworkshop.tddexample.service.exception.VozovozException;
 
-@org.springframework.stereotype.Service
-
+@Service
+@AllArgsConstructor
+@Slf4j
 public class UpdateProcessorService {
 
+    private final VozovozClient vozovozClient;
+    private final ManualExclusionService manualExclusionService;
+    private final AvailabilityNotifier availabilityNotifier;
+
     public void processUpdate(Update update) {
-        // TODO impl
+        if (update.getProductQuantity() <= 0) {
+            availabilityNotifier.notify(getNotAvailableProduct(update.getProductId()));
+            return;
+        }
+        if ("Blue".equals(update.getColor())) {
+            availabilityNotifier.notify(getNotAvailableProduct(update.getProductId()));
+            return;
+        }
+        if (!manualExclusionService.isProductEnabled(update.getProductId())) {
+            availabilityNotifier.notify(getNotAvailableProduct(update.getProductId()));
+            return;
+        }
+        try {
+            final boolean availableForTransportation = vozovozClient.isAvailableForTransportation(update.getProductId());
+            availabilityNotifier.notify(new ProductAvailability(update.getProductId(), availableForTransportation));
+        } catch (Exception exception) {
+            log.warn("Problems communicating with Vozovoz", exception);
+            throw new VozovozException();
+        }
+    }
+
+    private ProductAvailability getNotAvailableProduct(Long productId) {
+        return new ProductAvailability(productId, false);
     }
 
     public void disableProduct(long productId) {
