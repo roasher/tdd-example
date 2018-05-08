24f06dbf614a09203abac908100836341724596c
pyurkin <pavel.yurkin@leroymerlin.ru>
2018-05-08 16:58:50 +0300

    add VozovozClient implementation

--- src/main/java/ru/yurkinsworkshop/tddexample/client/VozovozClient.java
+++ src/main/java/ru/yurkinsworkshop/tddexample/client/VozovozClient.java
@@ -1,10 +1,20 @@
 package ru.yurkinsworkshop.tddexample.client;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import ru.yurkinsworkshop.tddexample.configuration.vozovoz.VozovozConfig;
 
 @Component
+@AllArgsConstructor
 public class VozovozClient {
+
+    private final RestTemplate vozovozRestTemplate;
+    private final VozovozConfig vozovozConfig;
+
     public boolean isAvailableForTransportation(Long productId) {
-        return false;
+        final String response = vozovozRestTemplate.getForObject(vozovozConfig.getEndpoint(), String.class, productId);
+        return Boolean.parseBoolean(response);
     }
+
 }
--- src/main/java/ru/yurkinsworkshop/tddexample/configuration/vozovoz/VozovozConfig.java
+++ src/main/java/ru/yurkinsworkshop/tddexample/configuration/vozovoz/VozovozConfig.java
@@ -4,10 +4,12 @@ import lombok.Data;
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.annotation.Validated;
 
 @Configuration
 @ConfigurationProperties(prefix = "vozovoz")
 @Data
+@Validated
 public class VozovozConfig {
 
     @NotBlank
